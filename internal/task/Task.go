2d5f0f8eafdcf875ce6ca0e734d54bb49aed8525
Brandon Dedolph <brandonjdedolph@gmail.com>
2026-03-16 14:34:52 -0500

    Task 1: task class struct created. Added validation on the status value

---
+++ internal/task/Task.go
@@ -0,0 +1,25 @@
+package task
+
+type Status string
+
+const (
+	StatusTODO       Status = "todo"
+	StatusInProgress Status = "inprogress"
+	StatusComplete   Status = "complete"
+	StatusUndefined  Status = ""
+)
+
+type Task struct {
+	Name        string
+	Description string
+	Status      Status
+	Priority    int
+}
+
+func (t Task) Validate() bool {
+	switch t.Status {
+	case StatusTODO, StatusInProgress, StatusComplete, StatusUndefined:
+		return true
+	}
+	return false
+}
