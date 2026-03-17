e64003e34ca195978a0d49d4bf882c5121539002
Brandon Dedolph <brandonjdedolph@gmail.com>
2026-03-16 21:03:06 -0500

    Created task list view.

---
+++ internal/tui/list.go
@@ -0,0 +1,71 @@
+package tui
+
+import (
+	"fmt"
+	"github.com/BrandonDedolph/task-manager/internal/service"
+	"github.com/BrandonDedolph/task-manager/internal/task"
+	tea "github.com/charmbracelet/bubbletea"
+	"strings"
+)
+
+type model struct {
+	tasks   []task.Task
+	cursor  int
+	service *service.TaskService
+}
+
+func NewModel(s *service.TaskService) (model, error) {
+	tasks, err := s.ListTasks()
+	if err != nil {
+		return model{}, err
+	}
+	return model{tasks: tasks, service: s}, nil
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.tasks)-1 {
+				m.cursor++
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	header := "Task Manager\n\n"
+
+	tasks := m.tasks
+
+	if tasks == nil {
+		return ""
+	}
+
+	var builder strings.Builder
+
+	for index, t := range tasks {
+		prepend := " "
+		if index == m.cursor {
+			prepend = ">"
+		}
+		fmt.Fprintf(&builder, "%s%s [%s]\n", prepend, t.Name, t.Status)
+	}
+
+	footer := "q: quit"
+
+	return header + builder.String() + footer
+}
+
